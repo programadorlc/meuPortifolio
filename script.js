e3ebe192019d167e8e79aeef328de61bb61990ea
Lucassilva1920 <lucasmacabu1920@gmail.com>
2023-05-14 01:26:16 -0300

    Adicionei 2 botões com a funcão de dark mode e light mode, mas para frente quando for aprendendo mas coisas, vou adionando aqui oque eu achar interessante

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert("Portifólio em produção")
+let body = document.getElementsByTagName("body")[0]
+
+function lightMode() {
+
+    onclick = body.style.backgroundColor = "#fff";
+    let icon = document.getElementsByClassName("icon")[0];
+    icon.style.backgroundColor = "#6060ff";
+}
+
+function darkMode() {
+
+    onclick = body.style.backgroundColor = "#101010";
+    let icon = document.getElementsByClassName("icon")[0];
+    icon.style.backgroundColor = "#212121";
+
+}
+
